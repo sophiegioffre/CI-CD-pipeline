545cf1bc64c153c76641709bbc2df4699b15b838
Sophie <sophie.gioff@gmail.com>
2022-09-20 19:21:54 -0400

    Fix bug

--- pipeline.test.js
+++ pipeline.test.js
@@ -1,6 +1,6 @@
 const pipeline = require("./pipeline");
 describe("My pipeline", () => {
   test("works", () => {
-    expect(pipeline.pipeline()).toEqual("CI/CD Pipline Exercise");
+    expect(pipeline.pipeline()).toEqual("Hello, CI/CD Pipline Exercise");
   });
 });
